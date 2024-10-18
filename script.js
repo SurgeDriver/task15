2871b007282eaf35d8cfb88a27e3cad74fcf7582
Pavel Sterhov <pasha.sterhow@yandex.ru>
2024-10-18 20:25:37 +0300

    15_1

--- script.js
+++ script.js
@@ -9,44 +9,17 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-
-const servicesGridItems = document.querySelectorAll('.services li');
+const servicesGrid = document.querySelector('.services');
 const servicesButton = document.querySelector('.services__expand-button');
 const servicesButtonText = servicesButton.querySelector('.expand-button__text');
 const servicesButtonIcon = servicesButton.querySelector('.expand-icon');
 
-let isExpanded = false;
-let limit = 6;
-
-function updateWithLimit() {
-  servicesGridItems.forEach((item, index) => { item.classList.remove('hidden'); });
-  for (let i = limit; i < servicesGridItems.length; i++) {
-    servicesGridItems[i].classList.add('hidden');
-  }
+let expanded = false;
+function SH() {
+  servicesButtonIcon.style.transform = expanded ? 'rotate(0deg)' : 'rotate(180deg)';
+  servicesButtonText.textContent = expanded ? "Скрыть" : "Показать все";
+  servicesGrid.style.maxHeight = expanded ? '164px' : '344px';
+  expanded = !expanded;
 }
 
-function screenChanged() {
-  limit = window.innerWidth <= 1120 ? 6 : 8;
-  if (!isExpanded) {
-    updateWithLimit();
-  }
-}
-
-window.addEventListener('resize', screenChanged);
-
-function showHide() {
-  isExpanded = !isExpanded;
-  servicesButtonIcon.style.transform = !isExpanded ? 'rotate(0deg)' : 'rotate(180deg)';
-  servicesButtonText.textContent = isExpanded ? "Скрыть" : "Показать все";
-  limit = window.innerWidth <= 1120 ? 6 : 8;
-  for (let i = limit; i < servicesGridItems.length; i++) {
-    servicesGridItems[i].classList.toggle('hidden');
-  }
-}
-
-limit = window.innerWidth <= 1120 ? 6 : 8;
-for (let i = limit; i < servicesGridItems.length; i++) {
-  servicesGridItems[i].classList.toggle('hidden');
-}
-
-servicesButton.addEventListener('click', showHide);
+servicesButton.addEventListener('click', SH);
